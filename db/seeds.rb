f58445745fc51a854ad5c095c38bea2a5afcc091
Julien Corion <astram59@gmail.com>
2020-11-18 16:44:03 +0100

    lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts 'creating 5 fake flats...'
+5.times do
+  flat = Flat.new(
+    name: ["awesome", "tiny", "lightful", "renoved"].sample + " flat",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(30..300),
+    number_of_guests: rand(1..9)
+  )
+  flat.save!
+end
+puts "finished!"
